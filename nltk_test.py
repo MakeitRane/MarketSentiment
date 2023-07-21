05a847c30b1a5db46fd8f587d550a76dba89a9db
Shreyaas14 <129126700+Shreyaas14@users.noreply.github.com>
2023-07-21 15:19:52 -0400

    Update nltk_test.py

--- nltk_test.py
+++ nltk_test.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import nltk
+from statistics import mean
+#nltk.download()
 
 text = """This is a test sentence. I love testing things because they're so wonderful. It's really insane how the standards for testing have grown over the years."""
 
@@ -25,8 +27,7 @@ from nltk.corpus import stopwords
 stop_words = set(stopwords.words('english')) #default list of stop words in a set to remove repetition
 #print(stop_words) 
 
-#CHANGE - removed text.lower
-words_tokenized = word_tokenize(text)
+words_tokenized = word_tokenize(text.lower())
 
 without_stop_words = []
 for word in words_tokenized:
@@ -36,30 +37,29 @@ for word in words_tokenized:
 
 
 #print the set of stop words which were excluded from the sample test sentence
-#print(set(words_tokenized) - set(without_stop_words))
+print(set(words_tokenized) - set(without_stop_words))
 
 #all words which are not stop words
-#print(without_stop_words)
+print(without_stop_words)
 
 
 
 # SENTIMENT ANALYSIS, STEMMING, LEMMATIZATION
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.sentiment import SentimentIntensityAnalyzer, SentimentAnalyzer
-demo_words = [""]
+demo_words = ["playing", "happiness", "going", "doing", "yes", "no", "I", "having", "had", "halved", "coding", "programming", "word", "rap", "genius", "go", "do"]
 
 #lemmatization considers the context and converts the word to its meaning base form aka the lemma
 #stemming removes the last few characters from a word
 lemmatizer = WordNetLemmatizer() 
 stemmer = PorterStemmer()
 
-#for word in demo_words:
+for word in demo_words:
     #left stem, right lemmatize
-    #print(word, stemmer.stem(word), lemmatizer.lemmatize(word, "v"))
+    print(word, stemmer.stem(word), lemmatizer.lemmatize(word, "v"))
 
 #sentiment analysis
 sent_int_anal = SentimentIntensityAnalyzer()
-
 
 crypto_slang = {
     "Fomo": -0.2, 
@@ -160,15 +160,10 @@ crypto_slang = {
 sent_int_anal.lexicon.update(crypto_slang)
 
 #prints scores for sentiment analysis
-print(sent_int_anal.polarity_scores("$BTC touched $30000 for the first time since April. We are so back. TO THE MOON."))
+def sent_analysis(tweet) -> float:
+    scores = sent_int_anal.polarity_scores(tweet)["compound"]
+    return scores
+
 #Why I believe $BTC will reach between $100K - $200K and $ETH $16K - $34K in the next bull run.
 #print(sent_int_anal.score_valence)
 #print(sent_int_anal.print_valence())
-
-
-
-
-
-
-
-
