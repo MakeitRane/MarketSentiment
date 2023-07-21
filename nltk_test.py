f9d621e447cd4aa95baaa6d53c54cbb0540d47b5
Anish <abr9956@nyu.edu>
2023-07-21 13:05:54 -0400

    finished coin convert

--- nltk_test.py
+++ nltk_test.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import nltk
-#nltk.download()
 
 text = """This is a test sentence. I love testing things because they're so wonderful. It's really insane how the standards for testing have grown over the years."""
 
@@ -26,7 +25,8 @@ from nltk.corpus import stopwords
 stop_words = set(stopwords.words('english')) #default list of stop words in a set to remove repetition
 #print(stop_words) 
 
-words_tokenized = word_tokenize(text.lower())
+#CHANGE - removed text.lower
+words_tokenized = word_tokenize(text)
 
 without_stop_words = []
 for word in words_tokenized:
@@ -36,32 +36,39 @@ for word in words_tokenized:
 
 
 #print the set of stop words which were excluded from the sample test sentence
-print(set(words_tokenized) - set(without_stop_words))
+#print(set(words_tokenized) - set(without_stop_words))
 
 #all words which are not stop words
-print(without_stop_words)
+#print(without_stop_words)
 
 
 
 # SENTIMENT ANALYSIS, STEMMING, LEMMATIZATION
 from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.sentiment import SentimentIntensityAnalyzer, SentimentAnalyzer
-demo_words = ["playing", "happiness", "going", "doing", "yes", "no", "I", "having", "had", "halved", "coding", "programming", "word", "rap", "genius", "go", "do"]
+demo_words = [""]
 
 #lemmatization considers the context and converts the word to its meaning base form aka the lemma
 #stemming removes the last few characters from a word
 lemmatizer = WordNetLemmatizer() 
 stemmer = PorterStemmer()
 
-for word in demo_words:
+#for word in demo_words:
     #left stem, right lemmatize
-    print(word, stemmer.stem(word), lemmatizer.lemmatize(word, "v"))
+    #print(word, stemmer.stem(word), lemmatizer.lemmatize(word, "v"))
 
 #sentiment analysis
 sent_int_anal = SentimentIntensityAnalyzer()
 
 #prints scores for sentiment analysis
-print(sent_int_anal.polarity_scores("Programming is fun"))
+print(sent_int_anal.polarity_scores("$BTC touched $30000 for the first time since April. We are so back. TO THE MOON."))
+#Why I believe $BTC will reach between $100K - $200K and $ETH $16K - $34K in the next bull run.
+#print(sent_int_anal.score_valence)
+#print(sent_int_anal.print_valence())
+
+
+
+
 
 
 
