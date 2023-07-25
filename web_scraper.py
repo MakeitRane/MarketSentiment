a2a57bffb2b2d6b1df3a28e64c02470ffc45f6d2
Shreyaas14 <129126700+Shreyaas14@users.noreply.github.com>
2023-07-25 15:58:45 -0400

    Update web_scraper.py
    added my fixes to the previous web scraper code and commented out current file

--- web_scraper.py
+++ web_scraper.py
@@ -1,4 +1,4 @@
-import requests
+"""import requests
 import pandas as pd
 twitter_data = []
 payload = {'api_key':'77dd07486d8909f02d4145c881e49f75', 'query': 'bitcoin', 'num': '5'}
@@ -14,4 +14,24 @@ def twitter_data(twitter_data):
         twitter_data.append({'ID': tweet['tweet_id'],'User': tweet["user"],'Tweet': tweet["text"],'URL': tweet["link"]})
     df = pd.DataFrame(twitter_data)
     df.to_json('scraped_tweets.json', orient='index')
-    return df
+    return df"""
+
+import requests
+import pandas as pd
+twitter_data = []
+
+def extract_twitter_data(): 
+    twitter_data = []
+    payload = {'api_key':'77dd07486d8909f02d4145c881e49f75', 'query': 'bitcoin', 'num': '5'}
+    response = requests.get('https://api.scraperapi.com/structured/twitter/search', params= payload)
+    data = response.json()
+    for result in data['organic_results']:
+        twitter_data.append({'Title': result['title'], 'Snippet': result['snippet'], 'URL': result['link']})
+    df = pd.DataFrame(twitter_data)
+    df.to_json('scraped_tweets.json', orient='index')
+    snippets = [result["snippet"] for result in data["organic_results"]]
+    return snippets
+
+
+
+
