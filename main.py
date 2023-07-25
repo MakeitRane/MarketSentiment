a4644b2f5fe71c7f0f8d9a90e19f7136e1c63bea
Shreyaas14 <129126700+Shreyaas14@users.noreply.github.com>
2023-07-25 15:59:17 -0400

    Update main.py
    main method now takes in snippets from search query and gives scores for each tweet

--- main.py
+++ main.py
@@ -1,50 +1,41 @@
 import requests
 import nltk
 import pandas as pd
-from web_scraper import twitter_data
+from web_scraper import extract_twitter_data
 from packages_test import sent_analysis
 
 def main():
-    coin_name = input("Want the market sentiment for a particular cryptocurrency? Type its ticker here: ")
+    #coin_name = input("Want the market sentiment for a particular cryptocurrency? Type its ticker here: ")
     pos_tweets = [] #positive tweets
     neg_tweets = [] #negative tweets
     neu_tweets = [] #neutral tweets
 
     #put coin name/ticker into twitter api search
-    tweets = twitter_data(coin_name)
+    tweets = extract_twitter_data()
 
     #while loop to process each tweet
-    curr_tweet_num = 0
+    #curr_tweet_num = 0
 
     #gets compound score and organizes it accordingly
-    while curr_tweet_num <= len(tweets):
-        curr_tweet = tweets[curr_tweet_num]
-        tweet_phrase = curr_tweet["ID", ["tweet"]][curr_tweet_num]
-        score = sent_analysis(tweet_phrase)
+    for tweet in tweets:
+        score = sent_analysis(tweet)
         if score == 0:
-            neu_tweets.append(tweet_phrase)
+            neu_tweets.append(tweet)
         elif score > 0:
-            pos_tweets.append([tweet_phrase, score])
+            pos_tweets.append([tweet, score])
         else:
-            neg_tweets.append([tweet_phrase, score])
-
-        curr_tweet_num += 1
+            neg_tweets.append([tweet, score])
 
     #number of positive, negative, and neutral tweets. can be used for further analysis or extra metrics
     num_pos_tweets = len(pos_tweets)
     num_neg_tweets = len(neg_tweets)
     num_neu_tweets = len(neu_tweets)
 
+    
+
     #to do: give final score between 0-10
     #idea: get average positive, negative, and neutral, whichever is highest multiply by 10 and that's the sentiment?
 
         
-
+main()
     
-
-
-
-
-
-
-
